1674ff0c8492f2e562627332564ab484f387790c
Taylor Morris <taylor.morris@gmail.com>
2018-04-28 15:27:15 -0700

    submission commit, added basic functionality concept to script.js and made minor html/css tweaks

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // ... as a developer, I want to make a data model (object constructor) for my portfolio data, so I can store individual projects and render them to the DOM.
-
-// roughly: json datastore -> new Project constructor -> jquery element selector from new Project
+// basics, roughly: json datastore -> new Project constructor -> jquery element selector from new Project
 // ???
 
-function Project(title, date, overview, long, main, addl) {
+"use strict";
+function Project(title, date, overview, imgs) {
   this.title = title;
   this.date = date;
   this.overview = overview;
   this.imgs = imgs;
 }
+
+// basic functionality //
+var projects = [];
+projects.push(new Project("Lab 01 Portfolio", "April 28, 2018", "First assignment", "img.jpg"));
 
 // long is main description for project page
 // overview is short description for index page
