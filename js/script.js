bc1673e3e16cf0d93d1c7ed4333a473da3a40978
Taylor Morris <taylor.morris@gmail.com>
2018-04-29 17:10:58 -0700

    lab-02-portfolio work, started using JS to render data in HTML, works only on index.html at the moment

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+'use strict';
+var projects = [];
+
+function Project (rawProjectObj) {
+  this.title = rawProjectObj.title;
+  this.date = rawProjectObj.date;
+  this.overview = rawProjectObj.overview;
+  this.imgs = rawProjectObj.imgs;
+  this.body = rawProjectObj.body;
+}
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('div.template').clone();
+  $newProject.removeClass('template');
+  $newProject.find('h1#title').html(this.title);
+  $newProject.find('h3.project-date').html(this.date)
+  return $newProject;
+};
+
+  rawProject.forEach(function(projectObject) {
+    // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+    projects.push(new Project(projectObject));
+  });
+
+  projects.forEach(function(project) {
+    $('#projects').append(project.toHtml());
+  });
+
+// projects.push(new Project("Lab 01 Portfolio", "April 28, 2018", "First assignment", "img.jpg"));
 // ... as a developer, I want to make a data model (object constructor) for my portfolio data, so I can store individual projects and render them to the DOM.
 // basics, roughly: json datastore -> new Project constructor -> jquery element selector from new Project
 // ???
-
-"use strict";
-function Project(title, date, overview, imgs) {
-  this.title = title;
-  this.date = date;
-  this.overview = overview;
-  this.imgs = imgs;
-}
-
-// basic functionality //
-var projects = [];
-projects.push(new Project("Lab 01 Portfolio", "April 28, 2018", "First assignment", "img.jpg"));
-
-// long is main description for project page
-// overview is short description for index page
-// main needs to be main image
-// addl needs to be additional images, as an array probably
