a3e25643a7a526ad2cff6c81e2a55439b4f99a68
Taylor Morris <taylor.morris@gmail.com>
2018-05-22 19:21:59 -0700

    started using node and express, added gitignore, updated readme, no style or structure changes

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require( 'express' );
+const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
+
+app.use( express.static('./public') );
+
+app.listen(PORT, function() {
+  console.log( `Listening on port: ${PORT}` );
+});
